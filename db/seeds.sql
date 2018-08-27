ac6d29b916bc949eae4ccb74bebdbd7397ed95ad
Katharine Arnold <katiekhs123@gmail.com>
2018-08-27 17:08:47 -0600

    set up

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+
+-- Insert a set of records.
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheesburger', false),
+    ('Baconator', false),
+    ('Veggie Burger', false);
+
+SELECT *
+FROM burgers;
---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
